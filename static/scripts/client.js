1ab75f6b74ac0153f9f24d0633a2e0454a9c73b4
Chris Clark <chris@untrod.com>
2012-01-21 23:40:29 -0500

    adding in last name and photo url to playlist details page

--- static/scripts/client.js
+++ static/scripts/client.js
@@ -210,7 +210,8 @@ $('#playlistItemDetails').live('pageshow', function(event){
 	spinnerStart();
 	selector = '#playlistItemDetailsTrackDetails'
 	buildTrackDetails(data, selector);
-	$(selector).append("<li>Picked by: " + data.first_name + "</li>");
+	$(selector).append("<li>Picked by: " + data.first_name + " " + data.last_name.substr(0,1) + ".</li>");
+	$(selector).append("<li><img src='" + data.photo_url + "'></li>");
 	$(selector).listview("refresh");
 	spinnerStop();
 });
