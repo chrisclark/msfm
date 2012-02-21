3a8450c7e58d5019380f9bba9f75281c86e71962
Chris Clark <chris@untrod.com>
2012-02-21 18:18:01 -0500

    minifying js, upgrading jqm, changing to .on() for admin.js

--- static/scripts/admin.js
+++ static/scripts/admin.js
@@ -9,36 +9,39 @@ var msfmAdmin = {
 				location.reload();
 			}
 		});
-	}	
+	},
+	markPlaying: function(pli_id) {
+		$.ajax({
+			type: "POST",
+			url: "/mark_playing",
+			data: "playlist_item_id=" + pli_id + '&location_id=' + msfm.locationId(),
+			success: function(data){
+				location.reload();
+			}
+		});
+	}
 };
 
 $(document).ready(function(){
 	"use strict";
 	msfm.isAdmin = true;
-});
-
-$("#btnMarkTopPlayed").unbind('click.msfm');
-$("#btnMarkTopPlayed").live('click.msfm', function(){
-	"use strict";
-	msfmAdmin.markPlayed($(".playlistItemButton").first().jqmData("playlist_item_id"));
-});
-
-$("#btnMarkPlayed").unbind('click.msfm');
-$("#btnMarkPlayed").live('click.msfm', function(){
-	"use strict";
-	msfmAdmin.markPlayed($("#pli_id").val());
-});
-
-$("#btnMarkPlaying").unbind('click.msfm');
-$("#btnMarkPlaying").live('click.msfm', function() {
-	"use strict";
-	var pli_id = $(".playlistItemButton").first().jqmData("playlist_item_id");
-	$.ajax({
-		type: "POST",
-		url: "/mark_playing",
-		data: "playlist_item_id=" + pli_id + '&location_id=' + msfm.locationId(),
-		success: function(data){
-			location.reload();
-		}
+	
+	$("#homePage").off('click', "#btnMarkTopPlayed");
+	$("#homePage").on('click', "#btnMarkTopPlayed", function(){
+		"use strict";
+		msfmAdmin.markPlayed($(".playlistItemButton").first().jqmData("playlist_item_id"));
+	});
+	
+	$("#homePage").off('click', "#btnMarkPlayed");
+	$("#homePage").on('click.msfm', "#btnMarkPlayed", function(){
+		"use strict";
+		msfmAdmin.markPlayed($("#pli_id").val());
+	});
+	
+	$("#homePage").off('click', "#btnMarkPlaying");
+	$("#homePage").on('click', "#btnMarkPlaying", function(){
+		"use strict";
+		var pli_id = $(".playlistItemButton").first().jqmData("playlist_item_id");
+		msfmAdmin.markPlaying(pli_id);
 	});
 });
